5b9ab3d3c0b446a8e05d7ee5aee0df1a3a2b554a
Kishorek <78011710+kishore600@users.noreply.github.com>
2025-06-24 08:53:21 +0530

    version updated

--- backend/controllers/version.controller.js
+++ backend/controllers/version.controller.js
@@ -1,6 +1,6 @@
 // controllers/version.controller.js
 const getLatestVersion = (req, res) => {
-  res.status(200).json({ latestVersion: "1.0.0.7" });
+  res.status(200).json({ latestVersion: "1.0.0.9" });
 };
 
 module.exports = { getLatestVersion };
